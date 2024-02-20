8523151e6605de488e0fd2f11280a5517560d3a4
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-02-20 15:56:19 -0800

    added new files

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers using Selection sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t min_index, i, j;
+	
+	if (array == NULL || size <= 1)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -29,7 +29,8 @@ void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif
