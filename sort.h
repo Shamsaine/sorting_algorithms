4fb210f6337b7d6086de507ba11573959ec141d3
Shamsudeen Ibrahim <shamsain3@gmail.com>
2024-02-20 04:37:23 -0800

    added new files

--- sort.h
+++ sort.h
@@ -30,7 +30,8 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
-
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void insertion_sort_list(listint_t **list);
 
 
 
